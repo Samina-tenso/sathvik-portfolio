baf126676521fc92b19bba4add44ed63b53efae6
Sathvik777 <katam.sathvik@gmail.com>
2024-05-08 18:06:06 +0200

    Add Sathvik specific content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,18 +31,17 @@ const Hero = () => {
                         animate={isInView ? "enter" : "hidden"} className=' pb-40 pt-30 flex flex-col items-center justify-center border-b-2 border-opacity-60 border-amber-100'
 
                     >
-
                         <div >
                             <div >
                                 <h1 className='text-h1 pb-10 md:max-w-[60vw]'>Sathvik Katam <br /> Data and ML expert </h1>
                             </div>
 
-                            <p className='text-ingress md:max-w-[60vw] xl:max-w-[35vw] mb-16'> Curiosity reduced Paul's fear to a manageable level.
-                                He heard truth in the old woman's voice, no denying it.
-                                If his mother stood guard out there ... if this were truly a
-                                test....
+                            <p className='text-ingress md:max-w-[60vw] xl:max-w-[35vw] mb-16'>
+                                Are you seeking a professional to drive transformative projects in machine learning and data engineering?
+                                With over 8 years of hands-on experience, I bring a proven track record of architecting cutting-edge solutions that harness the power of massive datasets and state-of-the-art machine learning algorithms.
                             </p>
-                            <Button url={'mailto:sammique95@gmail.com'} text={'Get in touch'} bgColor={'#FEF3C7'} textColor={'#1d4ed8'} />
+                            <Button url={'mailto:sathvik.katam@gmail.com'} text={'Mail'} bgColor={'#FEF3C7'} textColor={'#1d4ed8'} />
+                            <Button url={'tel:+46722324382'} text={'Tel'} bgColor={'#FEF3C7'} textColor={'#1d4ed8'} />
                         </div>
                     </motion.div >
 
