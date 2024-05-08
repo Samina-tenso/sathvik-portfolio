d49ec406c69c17e982b03211e0353ce4a41d3253
Samina <samina@Sathviks-MacBook-Pro.local>
2024-05-08 08:55:00 +0200

    Updates

---
+++ assets/post-two-BLTzSHB8.js
@@ -0,0 +1,49 @@
+const e=`---
+title: 'Rendering Dune part 2'
+url: 'rendering-dune'
+description: 'rendering posts with content from dune'
+pubDate: 6/3/2024
+published: true
+author: 'Samina Dahlberg'
+tags: ['react', 'tailwind', 'markdown]
+---
+# Exploring the Depths of Dune: An Epic Journey
+
+In Frank Herbert's masterpiece *Dune*, the sands of Arrakis conceal not only spice, but also a rich tapestry of wisdom and intrigue. Let's delve into some poignant excerpts from this timeless tale that continue to captivate readers across the universe.
+
+---
+
+## Bene Gesserit's Plans
+
+*"Plans within plans within plans."*
+
+The Bene Gesserit, with their intricate schemes and foresight, embody this maxim. Their manipulation of bloodlines and politics shapes the destiny of entire civilizations.
+
+## The Spice Melange
+
+*"He who controls the spice controls the universe."*
+
+The spice melange, a substance vital for space travel and prescient abilities, symbolizes power and dependency. Its allure and significance drive the narrative forward, fueling the ambitions of emperors and adventurers alike.
+
+## Muad'Dib's Ascendance
+
+*"I must not fear. Fear is the mind-killer."*
+
+Paul Atreides, also known as Muad'Dib, grapples with fear and destiny in his journey from exile to messiah. His transformation epitomizes the hero's journey, fraught with sacrifice and self-discovery.
+
+---
+
+\`\`\`python
+def calculate_spice_yield(planet, climate):
+    # Calculate spice yield based on planet's climate
+    if planet == "Arrakis":
+        if climate == "Desert":
+            return "High"
+        elif climate == "Temperate":
+            return "Moderate"
+        else:
+            return "Low"
+    else:
+        return "Negligible"
+
+        `;export{e as default};
